078ee916552400c7ff8674705591e8f24430a5b9
Juan Camilo <juanccamachob94@gmail.com>
2021-06-20 05:00:12 -0500

    unneccessary configuration removed

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ sequelize.authenticate()
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride('_method'));
 
 routes(app);
